5c1eae250c9e6a2d0a856d5319c257fabb4d313a
prince <parya3252@gmail.com>
2025-02-27 16:14:22 +0530

    new3

---
+++ math/arthmetic.py
@@ -0,0 +1,20 @@
+def add(a, b):
+    
+    return a + b
+
+def sub(a, b):
+    
+    return a - b
+
+def mul(a, b):
+   
+    return a * b
+
+def divide(a, b):
+    
+    if b == 0:
+        return "Error: Division by zero"
+    return a / b
+
+def mod(a,b):
+    return a//b
